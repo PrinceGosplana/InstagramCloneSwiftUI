1d276b811d907402c122ccbacf3f7135aff0c5a4
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-15 19:45:55 +0300

    init project

---
+++ InstagramCloneSwiftUI/InstagramCloneSwiftUI/InstagramCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSwiftUIApp.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
