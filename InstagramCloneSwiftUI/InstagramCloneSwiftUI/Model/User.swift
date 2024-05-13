a356ba21ae6b1ac97ccf2d24b090e00b8393ba21
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-13 16:15:20 +0300

    update user model

--- InstagramCloneSwiftUI/InstagramCloneSwiftUI/Model/User.swift
+++ InstagramCloneSwiftUI/InstagramCloneSwiftUI/Model/User.swift
@@ -16,14 +16,81 @@ struct User: Identifiable, Codable, Hashable {
     var bio: String?
     var email: String
     var isFollowed: Bool? = false
+    var stats: UserStats?
+}
+
+struct UserStats: Codable, Hashable {
+    var followingCount: Int
+    var followersCount: Int
+    var postsCount: Int
 }
 
 extension User {
     static let mockUsers: [User] = [
-        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF236E", userName: "Batman", profileImageUrl: "batman-2", fullName: "Bruce Wayne", bio: "Gotham's Dark Knight", email: "batman@gmail.com"),
-        .init(id: "31E9E3BE-C5FD-4B6D-8556-32AE8C5F3428", userName: "Venom", profileImageUrl: "venom-2", fullName: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
-        .init(id: "F8624FBC-076C-4619-9603-8812F03AF637", userName: "Ironman", profileImageUrl: "iron-man-1", fullName: "Tony Stark", bio: "Playboy $ Billionare", email: "ironman@gmail.com"),
-        .init(id: "832C4061-A929-4BC2-A5A7-B2603D72D6B4", userName: "Black Panther", profileImageUrl: "black-panther-2", fullName: "Bruce Wayne", bio: "Wakanda Forever", email: "blackpanther@gmail.com"),
-        .init(id: "2DC3A410-C31E-486A-B802-06A53967AAB5", userName: "Spiderman", profileImageUrl: "spiderman", fullName: "Peter Parker", bio: "Test bio", email: "spiderman@gmail.com")
+        .init(
+            id: "BC0B7A87-B914-47AA-A4A5-E37294CF236E",
+            userName: "Batman",
+            profileImageUrl: "batman-2",
+            fullName: "Bruce Wayne",
+            bio: "Gotham's Dark Knight",
+            email: "batman@gmail.com",
+            stats: UserStats(
+                followingCount: 1,
+                followersCount: 1,
+                postsCount: 1
+            )
+        ),
+        .init(
+            id: "31E9E3BE-C5FD-4B6D-8556-32AE8C5F3428",
+            userName: "Venom",
+            profileImageUrl: "venom-2",
+            fullName: "Eddie Brock",
+            bio: "Venom",
+            email: "venom@gmail.com",
+            stats: UserStats(
+                followingCount: 1,
+                followersCount: 12,
+                postsCount: 2
+            )
+        ),
+        .init(
+            id: "F8624FBC-076C-4619-9603-8812F03AF637",
+            userName: "Ironman",
+            profileImageUrl: "iron-man-1",
+            fullName: "Tony Stark",
+            bio: "Playboy $ Billionare",
+            email: "ironman@gmail.com",
+            stats: UserStats(
+                followingCount: 1,
+                followersCount: 1,
+                postsCount: 1
+            )
+        ),
+        .init(
+            id: "832C4061-A929-4BC2-A5A7-B2603D72D6B4",
+            userName: "Black Panther",
+            profileImageUrl: "black-panther-2",
+            fullName: "Bruce Wayne",
+            bio: "Wakanda Forever",
+            email: "blackpanther@gmail.com",
+            stats: UserStats(
+                followingCount: 1,
+                followersCount: 1,
+                postsCount: 1
+            )
+        ),
+        .init(
+            id: "2DC3A410-C31E-486A-B802-06A53967AAB5",
+            userName: "Spiderman",
+            profileImageUrl: "spiderman",
+            fullName: "Peter Parker",
+            bio: "Test bio",
+            email: "spiderman@gmail.com",
+            stats: UserStats(
+                followingCount: 1,
+                followersCount: 1,
+                postsCount: 1
+            )
+        )
     ]
 }
